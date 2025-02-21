2fa053580c43f712ed5d4d86fb11f6993db60f20
hrsh-hp <parmarharsh901@gmail.com>
2025-02-21 15:20:32 +0530

    Initial screen is done

---
+++ lib/Screens/initial.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:the_cakery/utils/constants.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
+  @override
+  void initState() {
+    super.initState();
+    // Initialize animation controller
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2), // Smooth fade-in animation
+    );
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(_animationController);
+
+    _animationController.forward();
+    _navigateToNextScreen();
+  }
+
+  void _navigateToNextScreen() async {
+    await Future.delayed(Duration(seconds: 3));
+    Constants.prefs = await SharedPreferences.getInstance();
+    bool isLoggedIn = Constants.prefs.getBool("isLoggedIn") ?? false;
+    Navigator.pushReplacementNamed(context, isLoggedIn ? "/home" : "/login");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(color: Colors.black26, blurRadius: 10, spreadRadius: 3),
+              ],
+            ),
+            child: ClipOval(
+              child: Image.asset("assets/Logo.png", fit: BoxFit.contain),
+            ),
+          ),
+        ),
+      ),
+    ); //3c2115
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:the_cakery/Screens/home.dart';
+import 'package:the_cakery/Screens/initial.dart';
+import 'package:the_cakery/Screens/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'THE CAKERY',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
+      ),
+      home: const InitialScreen(),
+      routes: {
+        '/home': (context) => HomeScreen(),
+        '/login': (context) => LoginScreen(),
+      },
+    );
+  }
+}
